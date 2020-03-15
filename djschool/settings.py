cdc27cb21d309679e4f1ebb0426122617a250cb2
Felix-Mutinda <1mutindafelix@gmail.com>
2020-03-15 18:17:01 +0300

    minor update

--- djschool/settings.py
+++ djschool/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'djschool.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
